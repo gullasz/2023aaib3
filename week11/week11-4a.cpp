2708b9fea2d42260c9beef058cf2d8da6dc54043
gullasz <sun9403022@gmail.com>
2024-11-20 11:53:50 +0800

    week11

---
+++ week11/week11-4a.cpp
@@ -0,0 +1,12 @@
+//week11-4a.cpp Part1: Input
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	while(cin >> n)//Part1: Input
+	{
+		if(n==0) break;
+		cout << n << endl;
+	}
+}
---
+++ week11/week11-5.cpp
@@ -0,0 +1,37 @@
+///week11-5.cpp 複習剝皮法
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n= 12345;
+    cout << "原本的n是: " << n << endl;
+    cout << "剝下最右邊的皮: " << n%10 << endl;
+    n/=10;
+    cout << "n變小了，現在的n是: " << n << endl;
+
+    cout << "原本的n是: " << n << endl;
+    cout << "剝下最右邊的皮: " << n%10 << endl;
+    n/=10;
+    cout << "n變小了，現在的n是: " << n << endl;
+
+    cout << "原本的n是: " << n << endl;
+    cout << "剝下最右邊的皮: " << n%10 << endl;
+    n/=10;
+    cout << "n變小了，現在的n是: " << n << endl;
+
+    cout << "原本的n是: " << n << endl;
+    cout << "剝下最右邊的皮: " << n%10 << endl;
+    n/=10;
+    cout << "n變小了，現在的n是: " << n << endl;
+
+    cout << "原本的n是: " << n << endl;
+    cout << "剝下最右邊的皮: " << n%10 << endl;
+    n/=10;
+    cout << "n變小了，現在的n是: " << n << endl;
+
+    cout << "原本的n是: " << n << endl;
+    cout << "剝下最右邊的皮: " << n%10 << endl;
+    n/=10;
+    cout << "n變小了，現在的n是: " << n << endl;
+
+}
